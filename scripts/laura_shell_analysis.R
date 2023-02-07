182a3e43d80541dcdd76ee4bf26e16806ba0a349
laura-fritschi <laura.maria.fritschi@gmx.ch>
2023-02-07 14:26:23 +0100

    adding script for analysis of shells and nocturnality to Maxs repository

---
+++ scripts/laura_shell_analysis.R
@@ -0,0 +1,12 @@
+library(lubridate)
+library(tidyr)
+library(ggplot2)
+library(gsheet)
+library(tictoc)
+library(data.table)
+library(dplyr)
+
+source("/Volumes/BZ/Scientific Data/RG-AS04-Data01/R_tracking_analysis/scripts/tracking_analysis_functions.R")
+
+## Set the directory to the '_analysis2' folder, or wherever all of the als files are located
+setwd("/Volumes/BZ/Scientific Data/RG-AS04-Data01/LCP/_analysis2/")
