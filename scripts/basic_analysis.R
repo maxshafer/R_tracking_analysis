432fadb80436072ccea5019cc2ff9ef2091f46e1
Maxwell Shafer <max.shafer@gmail.com>
2023-02-07 14:43:24 +0100

    Moving hybrid script to scripts folder

--- scripts/basic_analysis.R
+++ scripts/basic_analysis.R
@@ -9,7 +9,7 @@ library(dplyr)
 source("/Volumes/BZ/Scientific Data/RG-AS04-Data01/R_tracking_analysis/scripts/tracking_analysis_functions.R")
 
 ## Set the directory to the '_analysis2' folder, or wherever all of the als files are located
-setwd("/Volumes/BZ/Scientific Data/RG-AS04-Data01/LCP/_analysis2/")
+setwd("/Volumes/BZ/Scientific Data/RG-AS04-Data01/LCP/_analysis_Neomul_shell/")
 
 ####### LOAD IN GOOGLE SHEET DATA #######
 
