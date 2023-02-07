00c585a3bc1ddd1b8cc71a8c11ae4ceb62423622
Maxwell Shafer <max.shafer@gmail.com>
2023-02-07 13:55:38 +0100

    Adding findPeaks function and some fixes

--- scripts/tracking_analysis_functions.R
+++ scripts/tracking_analysis_functions.R
@@ -134,9 +134,31 @@ averageDay <- function(als_data = als_data, units = c("second", "minute", "halfh
   
 }
 
+
+## Run Python functions to find peaks
+## Remember, python is 0 indexed, so add 1 to the indices
+
+scipy.signal <- import("scipy.signal")
+
+## This function finds peaks using scipy.signal$find_peaks
+## Returns an als data frame with peaks labelled along with their prominences
+
+findPeaks <- function(als_data = als, distance = 4, prominence = 7) {
+  # This uses scipy to find peaks and returns something with a unique structure
+  peaks <- scipy.signal$find_peaks(x = als_data$mean_speed_mm, distance = distance, prominence = prominence)
+  
+  als_data$peaks <- "no"
+  als_data$peaks[peaks[[1]]+1] <- "yes"
+  als_data$peak_prominence <- NA
+  als_data$peak_prominence[peaks[[1]]+1] <- peaks[[2]]$prominences
+  
+  return(als_data)
+}
+
+
 ## This function takes als_data (for one individual) that already contains whether each entry is a peak (regular input is half_hour averaged)
 ## Intervals are kept inside (might slow it down, but keeps environment clear)
-returnPeakPercentages <- function(als_data = als_data, zoo_times = FALSE) {
+returnPeakPercentages <- function(als_data = als_data, zoo_times = FALSE, avg_days = FALSE) {
   
   dawn_intervals <- list()
   dusk_intervals <- list()
@@ -153,15 +175,31 @@ returnPeakPercentages <- function(als_data = als_data, zoo_times = FALSE) {
     }
   }
   
+  dawn_intervals <- dawn_intervals[2:8]
+  dusk_intervals <- dusk_intervals[2:8]
+  
+  if (avg_days) {
+    dawn_intervals <- list()
+    dusk_intervals <- list()
+    
+    if (zoo_times) {
+      dawn_intervals[[1]] <- interval(as.POSIXct(paste("1970-01-01 06:00:00", sep = ""), tz = "GMT"), as.POSIXct(paste("1970-01-01 07:30:00", sep = ""), tz = "GMT"))
+      dusk_intervals[[1]] <- interval(as.POSIXct(paste("1970-01-01 18:00:00", sep = ""), tz = "GMT"), as.POSIXct(paste("1970-01-01 19:30:00", sep = ""), tz = "GMT"))
+    } else {
+      dawn_intervals[[1]] <- interval(as.POSIXct(paste("1970-01-01 07:00:00", sep = ""), tz = "GMT"), as.POSIXct(paste("1970-01-01 08:30:00", sep = ""), tz = "GMT"))
+      dusk_intervals[[1]] <- interval(as.POSIXct(paste("1970-01-01 21:00:00", sep = ""), tz = "GMT"), as.POSIXct(paste("1970-01-01 22:30:00", sep = ""), tz = "GMT"))
+    }
+  }
+  
   # Calculate TRUE/FALSE for each interval
-  dawn_peaks <- lapply(dawn_intervals[2:8], function(x) {
+  dawn_peaks <- lapply(dawn_intervals, function(x) {
     df <- als_data[als_data$peaks == "yes",]
     output <- df$datetime %within% x
     output <- any(output)
     return(output)
   })
   
-  dusk_peaks <- lapply(dusk_intervals[2:8], function(x) {
+  dusk_peaks <- lapply(dusk_intervals, function(x) {
     df <- als_data[als_data$peaks == "yes",]
     output <- df$datetime %within% x
     output <- any(output)
